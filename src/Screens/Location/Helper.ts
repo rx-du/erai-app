efc768867c69a6d8221e24d6fbd8f20939095772
RazvanUrsatanu <razvan.ursatanu@equilobe.com>
2025-12-17 11:23:58 +0200

    added dark mode for android map

---
+++ src/Screens/Location/Helper.ts
@@ -0,0 +1,15 @@
+export const darkMapStyle = [
+  { elementType: 'geometry', stylers: [{ color: '#1d1d1d' }] },
+  { elementType: 'labels.text.stroke', stylers: [{ color: '#1d1d1d' }] },
+  { elementType: 'labels.text.fill', stylers: [{ color: '#8a8a8a' }] },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [{ color: '#2a2a2a' }],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [{ color: '#0f172a' }],
+  },
+];
--- src/Screens/Location/LocationScreen.tsx
+++ src/Screens/Location/LocationScreen.tsx
@@ -15,27 +15,12 @@ import ShareIcon from '../../Icons/share-32.svg';
 import GpsIcon from '../../Icons/gps-32.svg';
 import { useLoading } from '../../Context/LoadingContext';
 import { isAndroid } from '../../Constants/Device';
-
-const darkMapStyle = [
-  { elementType: 'geometry', stylers: [{ color: '#1d1d1d' }] },
-  { elementType: 'labels.text.stroke', stylers: [{ color: '#1d1d1d' }] },
-  { elementType: 'labels.text.fill', stylers: [{ color: '#8a8a8a' }] },
-  {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [{ color: '#2a2a2a' }],
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry',
-    stylers: [{ color: '#0f172a' }],
-  },
-];
+import { darkMapStyle } from './Helper';
 
 export default function LocationScreen() {
   const { theme, colors } = useTheme();
   const { t } = useTranslation();
-  const { region, addressTitle, addressDetails, permission, getLocation } = useGetLocation();
+  const { region, addressTitle, addressDetails, permission } = useGetLocation();
   const { shareLocation } = useShareLocation(region);
   const { showLoading, hideLoading } = useLoading();
 
@@ -43,55 +28,53 @@ export default function LocationScreen() {
 
   useEffect(() => {
     if (!region) {
-      showLoading('Loading location screen...');
-
-      const timer = setTimeout(() => {
-        hideLoading();
-      }, 500);
+      showLoading('Loading...');
 
       return () => {
-        clearTimeout(timer);
         hideLoading();
       };
     }
+
+    hideLoading();
   }, [region, showLoading, hideLoading]);
 
   if (permission === false) return <NoPermissionScreen />;
-  if (!region) return null;
 
   return (
     <MainLayout>
-      <View style={styles.container}>
-        <MapView
-          style={StyleSheet.absoluteFill}
-          initialRegion={region}
-          showsUserLocation
-          userInterfaceStyle={theme}
-          mapType="mutedStandard"
-          showsMyLocationButton={false}
-          customMapStyle={isAndroid && theme === 'dark' ? darkMapStyle : []}
-          ref={mapRef}
-        />
-
-        <CurrentLocationCard addressTitle={addressTitle} addressDetails={addressDetails} />
-
-        <View style={styles.buttonsContainer}>
-          <CustomButton
-            type="primary"
-            Icon={(props) => <ShareIcon {...props} color={colors.Text.accent.primary} />}
-            onPress={shareLocation}
-            width={48}
-            style={{ backgroundColor: colors.Bg.pure }}
+      {region && (
+        <View style={styles.container}>
+          <MapView
+            style={StyleSheet.absoluteFill}
+            initialRegion={region}
+            showsUserLocation
+            userInterfaceStyle={theme}
+            mapType="mutedStandard"
+            showsMyLocationButton={false}
+            customMapStyle={isAndroid && theme === 'dark' ? darkMapStyle : []}
+            ref={mapRef}
           />
-          <CustomButton
-            type="primary"
-            Icon={(props) => <GpsIcon {...props} color={colors.Text.accent.primary} />}
-            onPress={() => mapRef.current?.animateToRegion(region, 500)}
-            width={48}
-            style={{ backgroundColor: colors.Bg.pure }}
-          />
+
+          <CurrentLocationCard addressTitle={addressTitle} addressDetails={addressDetails} />
+
+          <View style={styles.buttonsContainer}>
+            <CustomButton
+              type="primary"
+              Icon={(props) => <ShareIcon {...props} color={colors.Text.accent.primary} />}
+              onPress={shareLocation}
+              width={48}
+              style={{ backgroundColor: colors.Bg.pure }}
+            />
+            <CustomButton
+              type="primary"
+              Icon={(props) => <GpsIcon {...props} color={colors.Text.accent.primary} />}
+              onPress={() => mapRef.current?.animateToRegion(region, 500)}
+              width={48}
+              style={{ backgroundColor: colors.Bg.pure }}
+            />
+          </View>
         </View>
-      </View>
+      )}
     </MainLayout>
   );
 }

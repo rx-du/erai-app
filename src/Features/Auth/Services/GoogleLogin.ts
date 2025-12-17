3cff8ba8673c022bf4d8d5fe65c4162f8fcdfda8
RazvanUrsatanu <razvan.ursatanu@equilobe.com>
2025-12-17 13:13:38 +0200

    deleted unnecessary webClientId

--- src/Features/Auth/Services/GoogleLogin.ts
+++ src/Features/Auth/Services/GoogleLogin.ts
@@ -1,9 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
-// these api keys are just for test
 GoogleSignin.configure({
-  webClientId: '611672989625-bbblgjq01o1p6gc5f580ksp1n3nmg86l.apps.googleusercontent.com',
   iosClientId: '611672989625-o7tdu6o5ibnkndkn4q34b0d1v2f4lide.apps.googleusercontent.com',
 });
 
